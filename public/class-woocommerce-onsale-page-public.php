c8cdb8ca69b3080e7c32e5cecb16a85728a00cac
MMajer <support@wpgenie.org>
2016-07-15 10:56:17 +0200

    first commit

---
+++ public/class-woocommerce-onsale-page-public.php
@@ -0,0 +1,151 @@
+<?php
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * @link       http://wpgenie.org
+ * @since      1.0.0
+ *
+ * @package    Woocommerce_onsale_page
+ * @subpackage Woocommerce_onsale_page/public
+ */
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    Woocommerce_onsale_page
+ * @subpackage Woocommerce_onsale_page/public
+ * @author     Your Name <email@example.com>
+ */
+class Woocommerce_onsale_page_Public {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $plugin_name       The name of the plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
+
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+
+	}
+	
+
+	public function template_loader( $template ) {		
+		
+		$find = array( 'woocommerce.php' );
+		$file = '';
+		
+		if ( is_post_type_archive( 'product' ) || is_page( wc_get_page_id( 'onsale' ) ) ) {
+
+			$file 	= 'archive-product.php';
+			$find[] = $file;
+			$find[] = WC()->template_path() . $file;
+
+		}
+
+		if ( !empty($file) ) {
+			
+			$template = locate_template( array_unique( $find ) );
+			
+			if ( ! $template || WC_TEMPLATE_DEBUG_MODE ) {
+				$template = WC()->plugin_path() . '/templates/' . $file;
+			}
+		}
+
+		return $template;
+	}
+
+
+	public function pre_get_posts( $q ) {
+
+		if(!$q->query)
+			return;
+
+		if(wc_get_page_id( 'onsale' ) != -1 ){
+			if( is_page( wc_get_page_id( 'onsale' ) ) ){
+				
+				$q->set( 'post_type', 'product' );
+				$q->set( 'page', '' );
+				$q->set( 'pagename', '' );			
+
+				// Fix conditional Functions
+				$q->is_archive           = true;
+				$q->is_post_type_archive = true;
+				$q->is_singular          = false;
+				$q->is_page              = false;
+				$q->is_sale_page         = true;
+			}
+		}	
+	}
+
+	public function mod_woocommerce_product_query($q, $WC_Query){
+		
+		global $wp_query;
+
+		if($wp_query->is_sale_page) {
+
+			$product_ids_on_sale = wc_get_product_ids_on_sale();
+			$meta_query = WC()->query->get_meta_query();
+			$q->set( 'post__in', array_merge( array( 0 ), $product_ids_on_sale ) );
+		}
+	}
+
+	public function woocommerce_page_title($title) {
+
+		global $wp_query;
+		
+		$onsale_page_id = wc_get_page_id( 'onsale' );
+
+
+		if($wp_query->is_sale_page) {
+			
+			$page_title   = get_the_title( $onsale_page_id );
+
+			return $page_title;
+
+		}
+
+		return $title;
+
+	}
+
+	public function woocommerce_get_breadcrumb($crumbs, $WC_Breadcrumb){
+		
+		global $wp_query;
+		
+		if($wp_query->is_sale_page) {
+			
+			$onsale_page_id = wc_get_page_id( 'onsale' );						
+			$crumbs[1] = array(get_the_title( $onsale_page_id ), get_permalink( $onsale_page_id )	);
+		}
+		
+		return $crumbs;
+
+	}	
+
+}
---
+++ readme.txt
@@ -0,0 +1,74 @@
+=== OnSale Page for WooCommerce ===
+Contributors: wpgenie2
+Donate link: http://wpgenie.org/
+Tags: wordpress onsale page, OnSale Page for WooCommerce, simple wordpress onsale page, onsale page, onsale page plugin, wordpress onsale page plugin, OnSale Page for WooCommerce plugin, simple onsale page, wpgenie onsale page, wpgenie
+Requires at least: 4.0
+Tested up to: 4.5
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+OnSale Page is an extension for Woocommerce which enables you to have real on sale page with paging, sorting and filtering.
+
+== Description ==
+
+OnSale Page for WooCommerce is an extension for Woocommerce. Since WooCommerce is popular we decided that it would be neat to 
+extend it with real WordPress page which displays products on sale. We developed this plugin because WooCommerce 
+has onsale widget and shortcode but it lacks paging, sorting and filtering which you can usually find on regular WooCommerce catalog page.
+
+With our onsale page plugin you can setup OnSale Page for WooCommerce where you can display all products that are on sale. 
+
+= Support =
+
+You can contact us at our website [wpgenie.org](http://wpgenie.org/) if you have problems or questions.
+
+
+== Installation ==
+
+= Minimum Requirements =
+
+* WordPress 4.0 or greater
+* WooCommerce 2.3 or greater
+
+
+= Setup =
+
+This section describes how to install OnSale Page for WooCommerce plugin and get it working.
+
+1. Upload the plugin files to the /wp-content/plugins/ directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress.
+3. Create new page.
+4. Go to Woocommerce Settings -> Products -> tab Display.
+5. Under Shop & Product Pages you will notice "Onsale Page".
+6. Select page you created for on sale page.
+7. Save settings.
+8. Go to new on sale page and you will see all on sale products there.
+
+If you have any problems contact us at http://wpgenie.org/
+
+
+
+
+== Frequently Asked Questions ==
+
+= Have a question? =
+
+If you want answer here please send us your questions to info@wpgenie.org
+
+
+
+== Screenshots ==
+
+1. OnSale Page plugin options
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
+
+
+== Upgrade Notice ==
+
+= 1.0 =
+Initial realease
